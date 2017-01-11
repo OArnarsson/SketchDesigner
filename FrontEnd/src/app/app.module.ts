a5198713add0bd537fabcf7e36850a59e2e9399e
Orri Arnarsson <orriarnars@gmail.com>
2017-01-11 13:38:34 +0000

    Basic canvas setup complete

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,18 +5,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DesignerComponent } from './designer/designer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: DesignerComponent},
+  {path: '**', component: DesignerComponent}
+]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DesignerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+DesignerComponent
+],
+imports: [
+BrowserModule,
+FormsModule,
+HttpModule,
+  RouterModule.forRoot(appRoutes)
+],
+providers: [],
+bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
--- FrontEnd/src/app/designer/designer.component.ts
+++ FrontEnd/src/app/designer/designer.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-designer',
-  templateUrl: './designer.component.html',
+  template: `<canvas #sketchCanvas class='sketchCanvas'
+  [attr.width]='_size'
+  [attr.height]='_size'></canvas>`,
   styleUrls: ['./designer.component.sass']
 })
-export class DesignerComponent implements OnInit {
 
-  constructor() { }
+export class DesignerComponent {
+  private _size: number;
 
-  ngOnInit() {
+  @ViewChild("sketchCanvas") sketchCanvas: ElementRef;
+
+  constructor(){
+    this._size = 150;
   }
 
+  ngAfterViewInit() {
+    let context: CanvasRenderingContext2D = this.sketchCanvas.nativeElement.getContext("2d");
+    context.fillStyle = 'green';
+    context.fillRect(0, 0, 150, 150);
+  }
+
+  get size(){
+    return this._size;
+  }
+
+  @Input () set size(newValue: number){
+    this._size = Math.floor(newValue);
+  }
 }
