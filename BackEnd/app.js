530162ba2a8679e78b30a349ab42c94ce8de795c
OArnarsson <orriarnars@gmail.com>
2017-01-18 12:12:44 +0000

    BackEnd file added
    Was missing in last commit

---
+++ BackEnd/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const api = require('./routes/api');
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('../FrontEnd/dist'));
+
+// Set API routes
+app.use('/api', api);
+
+// All other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile('FrontEnd/dist/index.html', { root: '../' });
+});
+
+// Server setup
+const port = process.env.PORT || '3000';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API running on localhost:${port}`));
