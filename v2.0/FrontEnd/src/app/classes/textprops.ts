5a833e9ff4af18340870b57fa6fa0e5154da28e9
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 14:10:52 +0000

    Textprops class implemented

--- v2.0/FrontEnd/src/app/classes/textprops.ts
+++ v2.0/FrontEnd/src/app/classes/textprops.ts
@@ -1,2 +1,15 @@
 export class Textprops {
+    public font: string;
+    public fontSize: number;
+    public allFonts: string[];
+    public italic: boolean;
+    public bold: boolean;
+
+    public constructor() {
+        this.fontSize = 30;
+        this.allFonts = ['Arial', 'Arial Black', 'Comic Sans MS', 'Impact', 'Lucida Sans Unicode', 'Tahoma', 'Titillium Web', 'Trebuchet MS', 'Verdana',];
+        this.font = this.allFonts[6];
+        this.italic = false;
+        this.bold = false;
+    }
 }
