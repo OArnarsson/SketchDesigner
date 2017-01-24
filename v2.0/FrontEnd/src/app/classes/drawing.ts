58167d2fd310108e241ea86034c8413a73c26d64
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 14:25:40 +0000

    Drawing class implemented

--- v2.0/FrontEnd/src/app/classes/drawing.ts
+++ v2.0/FrontEnd/src/app/classes/drawing.ts
@@ -1,2 +1,22 @@
+import {Position} from './position';
+import {Gui} from './gui';
+
+
 export class Drawing {
+    public gui: Gui;
+    public currPos: Position;
+
+    constructor (obj?) {
+        this.gui = new Gui();
+        this.currPos = new Position();
+
+        for (let prop in obj) {
+            if(prop == 'gui') {
+                this[prop] = obj[prop];
+            }
+            else {
+                this[prop] = new Position(obj[prop]);
+            }
+        }
+    }
 }
