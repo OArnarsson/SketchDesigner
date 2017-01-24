2e3800c0cb614ef3608acd509df65efb5ed908d4
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 14:01:25 +0000

    GUI class implemented

--- v2.0/FrontEnd/src/app/classes/gui.ts
+++ v2.0/FrontEnd/src/app/classes/gui.ts
@@ -1,2 +1,28 @@
+import {Textprops} from './textprops';
+
+
 export class Gui {
+    public strokeStyle: string;
+    public fillStyle: string;
+    public lineWidth: number;
+    public lineCap: string;
+    public lineJoin: string;
+    public tool: string;
+    public textprops: Textprops;
+    public opacity: number;
+    public hasFill: boolean;
+    public hasBorder: boolean;
+
+    public constructor() {
+        this.strokeStyle = "#000000";
+        this.lineCap = "round";
+        this.lineJoin = "round";
+        this.lineWidth = 5;
+        this.tool = 'pen';
+        this.opacity = 100;
+        this.hasFill = false;
+        this.hasBorder = true;
+        this.fillStyle = "#FFFFFF";
+        this.textprops = new Textprops();
+    }
 }
