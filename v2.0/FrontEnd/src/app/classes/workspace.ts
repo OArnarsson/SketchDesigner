16f2142cc74b1d232791f4b805f44be89e2354e9
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 14:30:30 +0000

    Workspace class implemented

---
+++ v2.0/FrontEnd/src/app/classes/workspace.ts
@@ -0,0 +1,28 @@
+import {Canvas} from './canvas';
+
+
+export class Workspace {
+    public canvases: Canvas[];
+    public title: string;
+    public dateModified: string;
+    public dateCreated: string;
+    //AuthO?
+
+    public constructor(obj?) {
+        this.canvases = [];
+        this.title = 'Untitled';
+        this.dateModified = '';
+        this.dateCreated = '';
+
+        for(let prop in obj) {
+            if(prop == 'canvases') {
+                for(let canvas of prop) {
+                    this.canvases.push(new Canvas(canvas));
+                }
+            }
+            else {
+                this[prop] = obj[prop];
+            }
+        }
+    }
+}
