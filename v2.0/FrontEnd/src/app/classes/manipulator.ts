83d287c34639d5d40625257f1584c7cbcdd43a8c
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 14:36:58 +0000

    Manipulator class implemented

--- v2.0/FrontEnd/src/app/classes/manipulator.ts
+++ v2.0/FrontEnd/src/app/classes/manipulator.ts
@@ -1,2 +1,22 @@
+import {Workspace} from './workspace';
+import {Canvas} from './canvas';
+import {Drawing} from './drawing';
+import {Gui} from './gui';
+
 export class Manipulator {
+    public workspace: Workspace;
+    public activeCanvas: Canvas;
+    public selectedDrawings: Drawing[];
+    public gui: Gui;
+    public moveX: number;
+    public moveY: number;
+
+    public constructor(workspace:Workspace) {
+        this.workspace = workspace;
+        this.activeCanvas = this.workspace.canvases[0];
+        this.selectedDrawings = [];
+        this.gui = new Gui();
+        this.moveX = 0;
+        this.moveY = 0;
+    }
 }
