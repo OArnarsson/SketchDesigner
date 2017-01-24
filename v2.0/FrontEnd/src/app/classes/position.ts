f05cb288dde57d7e4c13bc411064bbaa8be55689
Orri Arnarsson <orriarnars@gmail.com>
2017-01-24 13:41:07 +0000

    Position class implemented

--- v2.0/FrontEnd/src/app/classes/position.ts
+++ v2.0/FrontEnd/src/app/classes/position.ts
@@ -1,2 +1,24 @@
 export class Position {
+    public posX: number[];
+    public posY: number[];
+
+    public constructor(obj?) {
+        this.posX = [];
+        this.posY = [];
+        for (let prop in obj) this[prop] = obj[prop];
+    }
+
+    public pushPos(x, y) {
+        this.posX.push(x);
+        this.posY.push(y);
+    }
+
+    public movePos(tool, x, y) {
+        for (let posX of this.posX)
+            posX += x;
+        if(tool != 'square') {
+            for (let posY of this.posY)
+                posY += y;
+        }
+    }
 }
